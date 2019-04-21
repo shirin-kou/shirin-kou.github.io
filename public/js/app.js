056d9be34cbafad402abc8622a57ed56f1a9e0b5
shirin-kou <tzu.lin1320@gmail.com>
2019-04-21 10:54:33 +0900

    goal update

--- public/js/app.js
+++ public/js/app.js
@@ -2,61 +2,33 @@ $(function () {
   //処理
 });
 
+//express.static ミドルウェア関数に渡して、ファイル（imagesなど）を直接提供
 app.use(express.static("public"));
-// var newPostRef = firebase.database().ref();
-
-// // 日時を取得する関数
-// function ymd() {
-//   var date = new Date();
-//   return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds()
-// }
-
-// //名前をLocalStrageで保存する
-// //1.Save クリックイベント
-// $("#save").on("click", function () {
-//   var user_name = $("#name").val();
-//   var object = {
-//     user_name: user_name,
-//   };
-//   var json_obj = JSON.stringify(object);
-//   localStorage.setItem("username", json_obj);
-// });
-// //UserNameをクリアにする
-// $("#clear").on("click", function () {
-//   localStorage.removeItem("username");
-//   $("#name").val("");
-// });
-// //3.ページ読み込み：保存データ取得表示
-// if (localStorage.getItem("username")) {
-//   var obj = localStorage.getItem("username");
-//   var p_obj = JSON.parse(obj);
-//   $("#name").val(p_obj.user_name);
-// }
-
 
 //送信ボタンクリックでメッセージ送信
-// $(function () {
-//   $("#goal_1").on("click", function () {
-//     $('#text').val("代謝を上げるような生活アドバイスをします。がんばりましょう！");
-//     $("#goal_1").val("");
-//   });
-//   $("#goal_2").on("click", function () {
-//     $('#text').val("疲れを溜めずに、集中力が続く生活アドバイスをします。がんばりましょう！");
-//     $("#goal_2").val("");
-//   });
-//   $("#goal_3").on("click", function () {
-//     $('#text').val("夜ぐっすり眠れるような生活アドバイスをします。がんばりましょう！");
-//     $("#goal_3").val("");
-//   });
-//   $("#goal_4").on("click", function () {
-//     $('#text').val("体調に合わせた食事のアドバイスをします。がんばりましょう！");
-//     $("#goal_4").val("");
-//   });
-//   $("#goal_5").on("click", function () {
-//     $('#text').val("疲れを溜めずに、集中力が続く生活アドバイスをします。がんばりましょう！");
-//     $("#goal_5").val("");
-//   });
-// });
+$(function () {
+  $("#goal_1").on("click", function () {
+    $('#text').val("代謝を上げるような生活アドバイスをします。がんばりましょう！");
+    $("#goal_1").val("");
+  });
+  $("#goal_2").on("click", function () {
+    $('#text').val("疲れを溜めずに、集中力が続く生活アドバイスをします。がんばりましょう！");
+    $("#goal_2").val("");
+  });
+  $("#goal_3").on("click", function () {
+    $('#text').val("夜ぐっすり眠れるような生活アドバイスをします。がんばりましょう！");
+    $("#goal_3").val("");
+  });
+  $("#goal_4").on("click", function () {
+    $('#text').val("体調に合わせた食事のアドバイスをします。がんばりましょう！");
+    $("#goal_4").val("");
+  });
+  $("#goal_5").on("click", function () {
+    $('#text').val("疲れを溜めずに、集中力が続く生活アドバイスをします。がんばりましょう！");
+    $("#goal_5").val("");
+  });
+});
+
 // //メッセージが追加されたら自動的に表示
 // newPostRef.on('child_added', function (data) {
 //   var k = data.key;
